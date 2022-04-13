b4694112b5372d28eb957cf790e50613691817dc
Nithyanandh David, Xavier <nithyanandh.dx@listertechnologies.com>
2022-04-13 16:39:45 +0530

    added service url and port

--- index.js
+++ index.js
@@ -11,15 +11,14 @@ console.log(process.env);
 app.get("/", (_req, res) => {
     const content = `
         <h4>Sample express.js application</h4>
-        <h5>Added DAPR</h5>
-        <h6>Added Comm using DAPR</h6>
+        <p>Without DAPR - Inside subnets</p>
         <p>/backend - communicate with backend container</p>
     `
     res.send(content);
 });
 
 app.get("/backend", async (_req, res) => {
-    const request = axios.get(`http://dapr-app-id:nodeservice@localhost:${process.env.DAPR_HTTP_PORT}/test`);
+    const request = axios.get(`http://${process.env.SERVICE_APP_HOST}:${process.env.SERVICE_APP_PORT}/test`);
     request.then((_res) => {
             res.json(_res.data);
         }).catch((err) => {
